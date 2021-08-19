50abff3527fa0a6a0c30fee7d21a0e21ce803117
shnbetul <86834252+shnbetul@users.noreply.github.com>
2021-08-19 21:34:12 +0300

    Update app.php

--- app.php
+++ app.php
@@ -1,6 +1,22 @@
 <?php
 
 $age = 25;
+if($age>=0 && $age<=14){
+    echo "Çocuk";}
+
+else if($age>=15 && $age<=24){
+    echo "GEnç";}
+
+else if($age>=25 && $age<=64){
+    echo "Yetişkin";
+}
+
+else if($age>=65){
+    echo "Yaşlı";
+}
+else{
+    echo "Lütfen bir sayı yazınız";
+}
 
 /**
  * Bu ödevde age değişkeni içerisinde yer alan yaş değerinin bulunduğu
